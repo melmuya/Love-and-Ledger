3006c8603e342764b95a9da4c61f6e535c76ec0f
Mel <melchizedek.maranga@gmail.com>
2025-06-11 13:16:28 +0300

    feat: register models with Django admin for web-based data management

--- server/wedding_budget/admin.py
+++ server/wedding_budget/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Budget, Category, Vendor, ExpenseItem
 
 # Register your models here.
+admin.site.register(Budget)
+admin.site.register(Category)
+admin.site.register(Vendor)
+admin.site.register(ExpenseItem)
